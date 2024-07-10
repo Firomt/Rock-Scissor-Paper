faf3743a32df0f97bcad913d6a1115d1127e3cd9
Firomt <firomsamt@gmail.com>
2024-07-10 17:00:09 +0300

    add functions to get human input and to determine the winner

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
+// let humanScore=0;
+// let computerScore=0;
+
 function getComputerChoice(){
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+function getHumanChoice(){
+    let choice = prompt("Please enter your choice: rock, scissor, or paper: ").toLowerCase();
+    return choice;
+
+}
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice) {
+        return 'It\'s a tie!';
+    }
+    
+    if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper') ||
+        (humanChoice === 'paper' && computerChoice === 'rock')
+    ) {
+        console.log(`You chose: ${humanChoice}`);
+        console.log(`Computer chose: ${computerChoice}`);
+        console.log('You Win!')
+    } else {
+        console.log(`You chose: ${humanChoice}`);
+        console.log(`Computer chose: ${computerChoice}`);
+        console.log('Computer wins!');
+    }
+
+}
+const humanSelection= getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
+
+
